e3f3302cd4f5138a1202816ed8ae0dfeff183709
Kevin Barkevich <kbarkevich@gmail.com>
2020-08-28 15:29:40 -0400

    Added XOR training set

--- Program.cs
+++ Program.cs
@@ -46,14 +46,28 @@ namespace CSharp_Neural_Network
             NOT_SET[1].ins = new bool[] { true };
             NOT_SET[1].outs = new bool[] { false };
 
+            // Set to train the neural network on the XOR function.
+            TrainingInput[] XOR_SET = new TrainingInput[4];
+            XOR_SET[0].ins = new bool[] { false, false };
+            XOR_SET[0].outs = new bool[] { false };
+
+            XOR_SET[1].ins = new bool[] { false, true };
+            XOR_SET[1].outs = new bool[] { true };
+
+            XOR_SET[2].ins = new bool[] { true, false };
+            XOR_SET[2].outs = new bool[] { true };
+
+            XOR_SET[3].ins = new bool[] { true, true };
+            XOR_SET[3].outs = new bool[] { false };
+
 
             TrainingSet TRAINING_SET = new TrainingSet();
-            TRAINING_SET.set = AND_SET;  // Choose set to train on, or make your own (linearly seperable) function set
+            TRAINING_SET.set = XOR_SET;  // Choose set to train on, or make your own (linearly seperable) function set
 
             Console.WriteLine("Generating Neural Network...");
             NeuralNetwork network = new NeuralNetwork(0.2, 2, 1, 2, 3, Perceptron.FUNCTION_TYPE.SIGMOID, true);
             Console.WriteLine("Generated!");
-            network.Train(TRAINING_SET, 0.90, false);
+            network.Train(TRAINING_SET, 0.90, true);
         }
     }
 }
